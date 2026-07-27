2ca4d2d196939c5b36cd842ad0f712c9d49f6161
minhaloja-online <ferreira.bws@gmail.com>
2026-07-27 17:26:58 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+/* ============================================================
+   CONFIGURAÇÃO DO FIREBASE — projeto: mercantil-sousa
+   ============================================================ */
 const firebaseConfig = {
   apiKey: "AIzaSyAEI6ELlMfg3qRgj2XWpwgCmVdiDSKAsKY",
   authDomain: "mercantil-sousa.firebaseapp.com",
@@ -12,6 +9,3 @@ const firebaseConfig = {
   messagingSenderId: "263072246666",
   appId: "1:263072246666:web:035ac14517ea58bdbc3673"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
